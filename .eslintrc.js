44a0bc08adfab0ed35fb49fb4ad2d70ac67cc8d5
Marvin Zhang <tikazyq@163.com>
2021-12-14 10:47:18 +0800

    fixed typescript issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,10 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/multi-word-component-names': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/return-in-computed-property': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/no-explicit-any': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/explicit-module-boundary-types': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/ban-ts-ignore': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/ban-ts-comment': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   }
 }
